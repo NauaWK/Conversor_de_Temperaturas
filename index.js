c13dc35db2992ffc79515aa5f5d8ad6b876190a7
Nauã_WK <106595596+NauaWK@users.noreply.github.com>
2025-06-23 17:58:34 -0300

    att

--- index.js
+++ index.js
@@ -2,23 +2,26 @@ const submitBtn = document.getElementById("submitBtn")
 const userInputDiv = document.getElementById("userInputDiv")
 const resetBtn = document.getElementById("resetBtn")
 const typeOfConversionText = document.getElementById("typeOfConversion")
-let selectedOption = document.querySelector('input[name="option"]:checked');
+let selectedOption = document.querySelector('input[name="option"]:checked')
+let label = document.querySelector(`label[for="${selectedOption.id}"]`)
 
 function toggleVisibility(elements, display){
-    elements.forEach(element => {
+    elements.forEach(element =>{
         element.style.display = display
     });
 }
+
 function showUserInput(){
-    selectedOption = document.querySelector('input[name="option"]:checked');
+    selectedOption = document.querySelector('input[name="option"]:checked')
     if (!selectedOption){
         return
     }
     else{
         toggleVisibility([userInputDiv], "block")
-        typeOfConversionText.textContent = `${selectedOption.id}`
+        typeOfConversionText.textContent = label.textContent
     }
 }
+
 function hideUserInput(){
     toggleVisibility([userInputDiv], "none")
 }
