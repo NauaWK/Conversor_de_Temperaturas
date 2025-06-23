2e31c8e7501b7066181fbfb17af1d07f761c6c9f
Nauã_WK <106595596+NauaWK@users.noreply.github.com>
2025-06-23 15:46:31 -0300

    att

---
+++ index.js
@@ -0,0 +1,12 @@
+const submitBtn = document.getElementById("submitBtn")
+const userInputDiv = document.getElementById("userInputDiv")
+
+function toggleVisibility([elements], display){
+    elements.array.forEach(element => {
+        element.style.display = display
+    });
+}
+
+function showUserInput(){
+    
+}
