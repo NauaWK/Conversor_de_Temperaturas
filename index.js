326f776a092ad43468e543562564ead84e2c4da7
Nauã_WK <106595596+NauaWK@users.noreply.github.com>
2025-06-23 18:32:07 -0300

    att

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const submitBtn = document.getElementById("submitBtn")
 const userInputDiv = document.getElementById("userInputDiv")
 const resetBtn = document.getElementById("resetBtn")
 const typeOfConversionText = document.getElementById("typeOfConversion")
+const radios = document.querySelectorAll(".option")
 let label = document.querySelector(`label[for="${radio.id}"]`)
+
 
 
 function toggleVisibility(elements, display){
@@ -12,7 +14,6 @@ function toggleVisibility(elements, display){
 }
 
 function showUserInput(){
-    const radios = document.querySelectorAll(".option")
     radios.forEach(input => {
             input.addEventListener("change", function(){
             if(this.checked){
