e9dbb42156ea59febbee02ca277a9bd54b9ceedf
Nauã_WK <106595596+NauaWK@users.noreply.github.com>
2025-06-23 16:02:08 -0300

    att

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const submitBtn = document.getElementById("submitBtn")
 const userInputDiv = document.getElementById("userInputDiv")
+const selectedOption = document.querySelector('input[name="option"]:checked');
 
 function toggleVisibility(elements, display){
     elements.forEach(element => {
         element.style.display = display
     });
 }
-
 function showUserInput(){
+    if(!selectedOption){
+        return
+    }
+    else{
     toggleVisibility([userInputDiv], "block")
+    }
 }
